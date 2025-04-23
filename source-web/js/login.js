3442e20fe82060c91465f00de8cdd53f87eeaa67
Rizki Permana <alehw456@gmail.com>
2025-04-23 16:25:22 +0000

    update

--- source-web/js/login.js
+++ source-web/js/login.js
@@ -1,13 +1,11 @@
 document.getElementById('loginForm').addEventListener('submit', function (e) {
   e.preventDefault();
-
   const username = document.getElementById('username').value.trim();
   const password = document.getElementById('password').value.trim();
 
   if (username === '' || password === '') {
     alert("Silakan isi username dan password.");
   } else {
-    
-    alert("Login berhasil (simulasi)"); 
+    window.location.href = "pilih-game.html";
   }
 });
