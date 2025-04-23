01e91956dc0ec54a7b8e94b29b05d8de654ca86e
Rizki Permana <alehw456@gmail.com>
2025-04-23 16:01:42 +0000

    update

--- source-web/js/login.js
+++ source-web/js/login.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
     if (username === '' || password === '') {
       alert("Silakan isi username dan password.");
     } else {
-      // Lanjutkan ke proses login
-      alert("Login berhasil (simulasi)"); // bisa diganti dengan logic login sesungguhnya
+    
+      alert("Login berhasil"); 
     }
   });
